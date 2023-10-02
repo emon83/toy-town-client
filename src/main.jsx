94c6e0411c680188bbd436b9eac66a07b3d079f8
Emon <emonhasan7650@gmail.com>
2023-10-03 01:45:55 +0600

    redux install and setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,19 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes.jsx";
 import AuthProviders from "./providers/AuthProviders";
 import { ThemeProvider } from "./providers/ThemeProvider";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthProviders>
-    <ThemeProvider>
-      <React.StrictMode>
-        <div className="max-w-[1921px] mx-auto">
-        <RouterProvider router={router} />
-        </div>
-      </React.StrictMode>
-    </ThemeProvider>
-  </AuthProviders>
+  <Provider store={store}>
+    <AuthProviders>
+      <ThemeProvider>
+        <React.StrictMode>
+          <div className="max-w-[1921px] mx-auto">
+            <RouterProvider router={router} />
+          </div>
+        </React.StrictMode>
+      </ThemeProvider>
+    </AuthProviders>
+  </Provider>
 );
